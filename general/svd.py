e09413f70cc72bfdeee1522b744577e83c8309ab
poke53280 <anders.topper@gmail.com>
2018-05-11 10:25:11 +0200

    A bit more on svd

--- general/svd.py
+++ general/svd.py
@@ -2,18 +2,48 @@
 
 import numpy as np
 
+def IsOrthogonal(A):
+    I = np.zeros(A.shape, dtype = float)
+    np.fill_diagonal(I, 1)
+
+    return np.isclose(np.dot(A, A.T), I).all()
+
+"""c"""
+
 LA = np.linalg
 
-a = np.array([[1, 3, 4], [5, 6, 9], [1, 2, 3], [7, 6, 8]])
+a = np.array([[1, 0, 0, 0, 2],
+              [0, 0, 3, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 2, 0, 0],
+              [0, 0, 0, 1, 0],
+              [0, 2, 0, 0, 0]])
+
+
 print(a)
 
-U, s, Vh = LA.svd(a, full_matrices=False)
+U, s, Vh = LA.svd(a, full_matrices=True)
 
-assert np.allclose(a, np.dot(U, np.dot(np.diag(s), Vh)))
+# Vh: Initial rotation
+# s : Scaling along the coordinate axes
+# U : Final rotation
+#
+# Holds with full_matrices False: assert np.allclose(a, np.dot(U, np.dot(np.diag(s), Vh)))
+#
+
+np.dot(U, U.T)
 
 
-s
-np.diag(s)
+np.dot(U[0], U[3])
+
 np.dot(Vh[1],Vh[2])
 
-U
+a.shape
+U.shape
+s.shape
+Vh.shape
+
+
+np.allclose(a, np.dot(U, np.dot(s, Vh)))
+
+
