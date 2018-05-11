dae1c0580f894008d790691667c696a1b19d5ed6
poke53280 <anders.topper@gmail.com>
2018-05-11 10:51:00 +0200

    Organizing text processing efforts. In progress

---
+++ Driver_Text.py
@@ -0,0 +1,33 @@
+#
+# Hub for
+#
+# Text processing and pipeline into models.
+# Non text problems using text problem pipelines
+#
+#
+# Describe and show code for:
+#
+# word and char n-grams.
+# nn
+# embeddings
+# general sequences of states
+#
+#
+# char gram => nn
+# wordbatch
+#
+# antti talking => text example
+#
+# Organize existing code.
+#
+# Take in code from Kaggle text competitions and CTF predictions.
+#
+# lstm_imdb.py: Sequences, keras, nn attempts. Embeddings on general event/interval sequences.
+#
+#
+#
+
+
+
+
+
