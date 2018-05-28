a1cdcfe88778b9f851a40e2dc6f2b0a4bdd06465
poke53280 <anders.topper@gmail.com>
2018-05-28 11:33:44 +0200

    A start on a study of auto encoders for the project purpose

---
+++ home_credit/AutoEncoding.py
@@ -0,0 +1,11 @@
+
+
+# https://blog.keras.io/building-autoencoders-in-keras.html
+
+# https://www.datacamp.com/community/tutorials/autoencoder-keras-tutorial
+
+# http://aqibsaeed.github.io/2017-09-14-denoising-autoencoder-market-basket-cf/
+
+# Groceries.csv
+#http://www.salemmarafi.com/wp-content/uploads/2014/03/groceries.csv
+
