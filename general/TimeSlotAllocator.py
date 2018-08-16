bee6083c62cc2a8bc2dffdc504d7a5c1973cd4e1
poke53280 <anders.topper@gmail.com>
2018-08-16 15:56:40 +0200

    In progress on time slot allocator with accumulation option

--- general/TimeSlotAllocator.py
+++ general/TimeSlotAllocator.py
@@ -4,17 +4,17 @@ import numpy as np
 # Values are input as sorted from big to small.
 # Slots are input with target values from big to small.
 #
-# Returned list sized like input slots. Values are indices into the sorted value array. -1 if no value found to match.
+# Returned list sized like input slots. Values are indices into the sorted value array. Sub lists may be empty - indicating empty slot.
 # Tolerance is max appected difference between condidate data value and slot target value for insertion to slot.
 # Slots are filled greedily - high values first.
 #
 # See example() for usage
 
 
-def slot_allocator(data, slots, tolerance, isVerbose):
+def slot_allocator_best_value(data, slots, tolerance, isVerbose):
     
-    assert data == sorted(data, reverse=True)
-    assert slots == sorted(slots, reverse=True)
+    assert data == sorted(data, reverse=True), "data sorted big to small"
+    assert slots == sorted(slots, reverse=True), "slots sorted big to small"
 
     filled_slot = []
 
@@ -54,7 +54,19 @@ def slot_allocator(data, slots, tolerance, isVerbose):
             current_slot_idx = current_slot_idx + 1
             current_value_idx = current_value_idx +1 
 
-    return filled_slot
+    assert len(filled_slot) == len(slots)
+
+
+    s_res = []
+
+    for s in filled_slot:
+        print(s)
+        if s == -1:
+            s_res.append([])
+        else:
+            s_res.append([s])
+
+    return s_res
 
 """c"""
 
@@ -80,27 +92,26 @@ def get_slot_data(data_provider, list_time_slots_configuration, rTimeTolerance, 
 
     list_time_values = data_provider.getTimeStamps()
 
-    l_slot_allocation = slot_allocator(list_time_values, list_time_slots_configuration, rTimeTolerance, isVerbose)
+    l_slot_allocation = slot_allocator_best_value(list_time_values, list_time_slots_configuration, rTimeTolerance, isVerbose)
 
     assert len(l_slot_allocation) == n_slots, "slot allocater bad return"
 
-    for i, alloc_location in enumerate(l_slot_allocation):
+    for i, alloc_location_list in enumerate(l_slot_allocation):
         iSlotIndex = i
-        iDataIndex = alloc_location
+        iDataIndexList = alloc_location_list
 
-        if iDataIndex == -1:
+        if len (alloc_location_list) == 0:
             if isVerbose: print(f"{iSlotIndex}: Empty")
 
         else:
-            assert (iDataIndex >= 0) and (iDataIndex <= len(list_time_values)), "index out of range"
+
+            for iDataIndex in alloc_location_list:
+                assert (iDataIndex >= 0) and (iDataIndex <= len(list_time_values)), "index out of range"
+
 
             timeSlotTargetValue = list_time_slots_configuration[iSlotIndex]
-            timeActualValue = list_time_values[iDataIndex]
-            valueOffset = timeActualValue - timeSlotTargetValue
-            if isVerbose: print(f"{iSlotIndex}: Insert data idx = {iDataIndex}. Values {timeActualValue} => {timeSlotTargetValue}. value offset= {valueOffset}")
 
-            # Request data provider to prepare item at iDataIndex to offset valueOffset
-            data_record = data_provider.getTimeRecord(timeActualValue, valueOffset, data_per_element)
+            data_record = data_provider.getTimeRecord(timeSlotTargetValue, alloc_location_list, data_per_element)
 
             assert data_record.shape[0] == data_per_element, "data record size incorrect"
 
@@ -118,6 +129,8 @@ def example():
     values = [90, 80, 4, -1]
     slots = [40, 40, 3, 3]
 
-    slot_allocator(values, slots, 5, False)
+    slot_allocator_best_value(values, slots, 5, False)
     # => [-1, -1, 2, 3]
+
+
 
--- general/TimeSlotAllocator_example.py
+++ general/TimeSlotAllocator_example.py
@@ -14,35 +14,111 @@ os.chdir('C:\\Users\\T149900\\ml_mercari')
 from general.TimeSlotAllocator import get_slot_data
 
 
-def TimeSlotAllocator_example():
 
-    class DataProviderImpl:
 
-        # Time points, sorted from high to low for which data is available.
-        def getTimeStamps(self):
-            return [3,2,1, -4]
+class DataProviderImpl:
 
-        # Number of float32 elements for each time point.
-        def getDataPerElement(self):
-            return 2
+    # Time points, sorted from high to low for which data is available.
+    def getTimeStamps(self):
+        return [3,2,1, -4]
 
-        # Provide data for time point time_value adjusted with time offset valueOffset.
-        def getTimeRecord(self, time_value, valueOffset, data_size):
-            assert time_value in [3,2,1,-4]
+    # Number of float32 elements for each time point.
+    def getDataPerElement(self):
+        return 2
 
-            assert data_size == self.getDataPerElement(), "data_size == CreditCardBalanceRecord.staticGetDataSize()"
+    # Provide data for time point time_value adjusted with time offset valueOffset.
+    def getTimeRecord(self, time_slot_value, alloc_location_list, data_size):
 
-            # rec = ...
 
-            an = np.zeros(data_size, dtype = np.float32)
+        data_points = self.getTimeStamps()
 
-            return an
+        for x in alloc_location_list:
+            assert x >= 0 and x < len(self.getTimeStamps()), "time stamp index out of range"
 
-    """c"""
+            print(f" Requesting element idx = {x}, value = {data_points[x]}")
+        
 
-    b = DataProviderImpl()
+        an = np.zeros(data_size, dtype = np.float32)
 
-    b.getTimeRecord(-4, 0, 2)
+        return an
+
+"""c"""
+
+    
+b = DataProviderImpl()
+
+b.getTimeRecord(3, [0,3], 2)
+
+
+values = [90, 80, 4, 3, -1]
+slots = [40, 40, 3, 3]
+
+
+slot_allocator_best_value(values, slots, 5, True)
+
+slot_allocator_multi_value(values, slots, 5)
+
+
+b.getTimeStamps()
+
+
+d = get_slot_data(b, [40, 40, 3, 3], 5, True)
+
+
+
+# Find nearest value in slot for values
+# First make work on ascending order;
+
+# Slots must be unique valued
+
+import bisect
+
+values = [-1, 3, 4, 4.9,5.5, 6, 6, 7]
+slots = [3,4,5, 6]
+
+
+
+def slot_allocator_multi_value(values, slots, rTolerance):
+
+    s_res = []
+
+    for s in slots:
+        s_res.append([])
+
+    bisect_lo = 0
+
+
+    for idx, value in enumerate(values):
+
+        bisect_lo = bisect.bisect_left(slots, value, lo = bisect_lo, hi = len (slots))
+
+        if bisect_lo == len(slots):
+            slot_idx = len(slots) -1
+        else:
+            slot_idx = bisect_lo
+
+        slot_value = slots[slot_idx]
+
+
+        if np.abs(value - slot_value) > rTolerance:
+            pass
+        else:
+            s_res[slot_idx].append(idx)
+
+
+    return s_res    
+
+"""c"""
+    
+
+slot_allocator_multi_value(values, slots, 0.01)
+
+     
+
+
+
+
+
 
     slots = [-1,-2, -3, -4, -5.1, -6, -7, -8, -9]
     rTolerance = 0.9
@@ -53,6 +129,10 @@ def TimeSlotAllocator_example():
 
 """c"""
 
+# DataProviderImpl provides data at times 3, 2, 1, -4
+
+#
 
 
+TimeSlotAllocator_example()
 
