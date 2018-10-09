51da3541ff2b3b0d5676ca9a67bd3146de845209
poke53280 <anders.topper@gmail.com>
2018-10-09 09:13:55 +0200

    Completed DataSQLParser Completed DataSQLParser v1

--- p0/DataSQLParser.py
+++ p0/DataSQLParser.py
@@ -46,14 +46,23 @@ def get_dict(filename):
 
             isFindComment = False
 
-        if isSQL and not isFindComment:
+        elif isSQL and not isFindComment:
             c = c[:-1]
             queries.append(c)
             isFindComment = True
 
-
+        else:
+            if isSQL:
+                print(f"Skipped SQL line without prior comment: {c}")
+            elif isComment:
+                print(f"Skipped comment '{c}' because comment already given.")
+            else:
+                print(f"Skipped line '{c}'")
+            
     if len(keys) > len (queries):
         keys.pop()
+
+    assert len(keys) == len (queries)
 
     d = dict(zip(keys, queries))
 
@@ -67,4 +76,7 @@ if __name__ == "__main__":
     filename = "test_sql.txt"
     d = get_dict(filename)
 
+
+for key, query in d.items():
+    print(f"{key}: {query}")
 
