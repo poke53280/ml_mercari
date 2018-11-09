Fixed split, was target leakage

plasticc/plasticc_numba.py
@@ -130,7 +130,7 @@ def generate_fast(data_sample, a_all, y_all, p_all, begin_offset, lengths, num_o
 def generate_sets(df_meta, df, num_sets):
 
 
-    y = np.array (df_meta.target, dtype = np.int32)
+    
 
     idx = get_run_length_stops(df.object_id.values)
 
@@ -168,11 +168,11 @@ def generate_sets(df_meta, df, num_sets):
         generate_fast(data[start_idx:end_idx, :], a_all, y_all, p_all, begin_offset, lengths, num_objects)
 
     
-    y_tot = np.tile(y, num_sets)
+    y = np.tile(np.array (df_meta.target, dtype = np.int32), num_sets)
 
-    y = y_tot
+    id = np.tile(np.array (df_meta.object_id, dtype = np.int64), num_sets)
 
-    return data, y
+    return data, y, id
 """c"""
 
 
@@ -187,7 +187,7 @@ df = pd.read_csv(DATA_DIR + "training_set.csv")
 
 num_sets = 60
 
-data, y = generate_sets(df_meta, df, num_sets)
+data, y, ids = generate_sets(df_meta, df, num_sets)
 
 
 num_all = y.shape[0]
@@ -210,23 +210,37 @@ m = m0 | m1
 
 y = y[m]
 data = data[m]
+ids = ids[m]
 
 print(f"Element reduction {num_all} => 2 x {nTargetsForClass}")
 
-
-
 gc.collect()
 
-num_splits = 8
+
+
+
+
+
+
+num_splits = 10
+
+uniqueIDs = np.unique(ids)
+
 
 folds = KFold(n_splits=num_splits, shuffle=True, random_state=11)
 
 oof_preds = np.zeros(data.shape[0])
 
-for n_fold, (trn_idx, val_idx) in enumerate(folds.split(data)):
+for n_fold, (trn_idx_unique, val_idx_unique) in enumerate(folds.split(uniqueIDs)):
+
+    trn_ids = uniqueIDs[trn_idx_unique]
+    val_ids = uniqueIDs[val_idx_unique]
+
+    m_trn = np.in1d(ids, trn_ids) 
+    m_val = np.in1d(ids, val_ids)
         
-    trn_x, trn_y = data[trn_idx], y[trn_idx]
-    val_x, val_y = data[val_idx], y[val_idx]
+    trn_x, trn_y = data[m_trn], y[m_trn]
+    val_x, val_y = data[m_val], y[m_val]
 
     print (f"Fold {n_fold + 1} / {num_splits}")
 

plasticc/plasticc_stratify.py
@@ -14,24 +14,23 @@ def generate_single_target(df_meta_all, df_all, itarget, num_desired_elements):
 
     m_meta = (df_meta_all.target == itarget)
 
-    objs = np.unique(df_meta_all[m_meta].object_id)
+    df_meta = df_meta_all[m_meta].copy()
+
+    objs = df_meta.object_id.values
 
     m_data = df_all.object_id.isin(objs)
-
-    df_meta = df_meta_all[m_meta].copy()
+   
     df = df_all[m_data].copy()
 
     num_objects = df_meta.shape[0]
 
     num_sets = 1 + int (num_desired_elements / num_objects)
 
-    num_elements = num_sets * num_objects
+    d, y, id = generate_sets(df_meta, df, num_sets)
 
-    d0, y_0 = generate_sets(df_meta, df, num_sets)
+    assert d.shape[0] == num_sets * num_objects
 
-    assert d0.shape[0] == num_elements
-
-    return d0, y_0
+    return d, y, id
 
 """c"""
 
@@ -44,15 +43,17 @@ df_meta_all = pd.read_csv(DATA_DIR + "training_set_metadata.csv")
 df_all = pd.read_csv(DATA_DIR + "training_set.csv")
 
 
+num_desired_elements = 100000
 
-d0, y_0 = generate_single_target(df_meta_all, df_all, 52, 500000)
+d0, y_0, ids_0 = generate_single_target(df_meta_all, df_all, 52, num_desired_elements)
 
-d1, y_1 = generate_single_target(df_meta_all, df_all, 90, 500000)
+d1, y_1, ids_1 = generate_single_target(df_meta_all, df_all, 90, num_desired_elements)
 
 
 data = np.vstack([d0, d1])
 y = np.hstack([y_0, y_1])
 
+ids = np.hstack([ids_0, ids_1])
 
 
 