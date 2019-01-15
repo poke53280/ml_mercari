a8e9f87f75315f2c46a06dd7af68ab73f45a3193
poke53280 <anders.topper@gmail.com>
2019-01-15 12:23:30 +0100

    Ordliste parsing

--- Train_Index_Group.py
+++ Train_Index_Group.py
@@ -4,6 +4,61 @@ import pandas as pd
 import numpy as np
 
 
+id = [0,0,1,1,3, 0]
+d = [3,3,4,5,6, 3]
+s = [4,4,4,4,4, 4]
+ix = [7,2, 0, 3, 1, 4]
+
+t = ['A', 'B', 'C', 'D', 'E', 'C']
+
+df = pd.DataFrame({'id': id, 'd' : d, 's': s, 'idx': ix, 't':t})
+
+df
+
+# Group by id, d, s. Check t ordering.
+
+df_grouped = df.groupby(['id', 'd', 's'])
+
+for group_key, item in df_grouped:
+    print(group_key)
+    print(item)
+"""c"""
+
+
+g_id = []
+g_d = []
+g_s = []
+g_t = []
+
+for key, item in df_grouped:
+
+    print (key)
+
+    g = df_grouped.get_group(key)
+
+    l = list (g.t)
+
+    s = ".".join(l)
+
+    g_id.append(key[0])
+    g_d.append(key[1])
+    g_s.append(key[2])
+
+    g_t.append(s)
+    
+"""c"""
+
+df_g = pd.DataFrame({'id': g_id, 'd':g_d, 's': g_s, 't' : g_t})
+
+df_g
+
+for x in g:
+    print (x.index)
+    print (x.names)
+
+
+
+
 
 def full_sum(df):
     l_b = list()
--- txt/txt.py
+++ txt/txt.py
@@ -1,4 +1,32 @@
 
+###########################################################
+
+from nltk.stem.snowball import NorwegianStemmer
+
+stemmer = NorwegianStemmer()
+stemmer.stem("bilene")
+
+###########################################################
+
+from spacy.lang.nb import Norwegian
+nlp = Norwegian()
+
+doc = nlp(u'Dette er en setning.')
+
+for token in doc:
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+          token.shape_, token.is_alpha, token.is_stop)
+
+##########################################################
+
+from gensim.models.wrappers import FastText
+
+
+
+
+
+
+
 
 import numpy as np
 import pandas as pd
@@ -13,3 +41,42 @@ df_c = pd.DataFrame(df.get_chunk(1500))
 df_c = df_c.reset_index()
 
 df_c
+
+
+# Vanlige ord
+
+DATA_DIR = "C:\\NORDBANK\\"
+
+
+df_bm = pd.read_csv(DATA_DIR + '\\ordbank_bm\\fullform_bm.txt', skiprows = 25, encoding = 'latin-1', error_bad_lines = False, warn_bad_lines = True, sep = '\t')
+df_bm.columns = ['A', 'B', 'C', 'D', 'E', 'F']
+
+
+df_nn = pd.read_csv(DATA_DIR + '\\ordbank_nn\\fullform_nn.txt', skiprows = 25, encoding = 'latin-1', error_bad_lines = False, warn_bad_lines = True, sep = '\t')
+df_nn.columns = ['A', 'B', 'C', 'D', 'E', 'F']
+
+
+df = pd.concat([df_bm, df_nn], axis = 0)
+
+df.B = df.B.fillna('tom')
+df.C = df.C.fillna('tom')
+
+s = set (df.B) | set (df.C)
+
+# Remove and shorter
+
+l = list (s)
+
+l_long = []
+
+for x in l:
+    if len(x) > 2:
+        l_long.append(x)
+"""c"""
+
+s = set(l_long)
+
+
+
+
+
