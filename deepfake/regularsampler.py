55268700f9e359618d5d8453b7f627c948162370
poke53280 <anders.topper@gmail.com>
2020-02-20 20:05:36 +0100

    square sampling complete

--- deepfake/regularsampler.py
+++ deepfake/regularsampler.py
@@ -25,21 +25,30 @@ import cv2
 
 ######################################################################
 #
-#   etcf
+#   draw_grid
 #
 
-def etcf (mtcnn_detector, image, face, isDraw):
+def draw_grid(image_in, out):
+    image = image_in.copy()
+    for x in range(raster_w):
+        for y in range(raster_h):
+            p = out[x, y]
+            _draw_box_integer_coords(image, p[0], p[1], 1)
 
-    if isDraw:
-        mtcnn_detector.draw(image, [face])
+    imgplot = plt.imshow(image)
+    plt.show()
 
-    rW = 2.0  # Feature slack 
 
-    raster_w = 10
-    raster_h = 3
+######################################################################
+#
+#   get_sample_grid
+#
 
-    assert raster_h > 1
+def get_sample_grid (face, rW, raster_w, raster_h):
 
+    assert raster_h > 0
+
+    rH = raster_h/ raster_w
 
     anFeatureSet = get_feature_sets()
 
@@ -64,17 +73,16 @@ def etcf (mtcnn_detector, image, face, isDraw):
     pB = pA + vector
     pC = pA + vector + vector_p
     pD = pA + vector_p
-
+    
+    #  D    C
     #
-    #  D       C
-    #
-    #  A       B
-    #
+    #  A    B
 
     # Rasterize
 
-    step_x_vector = vector / (raster_w -1 )
-    step_y_vector = vector_p / (raster_h -1 )
+    step_x_vector = vector / (raster_w -1)
+
+    step_y_vector = 0 if raster_h == 1 else vector_p / (raster_h -1)
 
     anOut = np.zeros((raster_w, raster_h, 2), dtype = np.int32)
 
@@ -84,16 +92,71 @@ def etcf (mtcnn_detector, image, face, isDraw):
             p = pA + x * step_x_vector + y * step_y_vector
             anOut[x, y] = p.astype(np.int32)
 
-    if isDraw:
-        for x in range(raster_w):
-            for y in range(raster_h):
-                p = anOut[x, y]
-                _draw_box_integer_coords(image, p[0], p[1], 1)
-
-        imgplot = plt.imshow(image)
-        plt.show()
-
     return anOut
+
+
+
+
+######################################################################
+#
+#   sample
+#
+
+def sample(video, mtcnn_detector, rW, raster_w, raster_h, isDraw):
+    faces = find_two_consistent_faces(mtcnn_detector, video)
+
+    anBegin = get_sample_grid(faces[0], rW, raster_w, raster_h)
+
+    if isDraw:
+        draw_grid(video[0], anBegin)
+
+    anEnd   = get_sample_grid(faces[1], rW, raster_w, raster_h)
+
+    if isDraw:
+        draw_grid(video[31], anEnd)
+
+    aiGrid = np.zeros((raster_w, raster_h, 32, 3), dtype = np.int32)
+
+    for x in range(raster_w):
+        for y in range(raster_h):
+            p0 = anBegin[x, y]
+            p1 = anEnd[x, y]
+            l = get_line(np.array((*p0, 0)), np.array((*p1, 31)))
+
+            assert l.shape[1] >= 32
+
+            l = np.swapaxes(l, 0, 1)
+            l = l[:32]
+            l = l.astype(np.int32)
+
+            aiGrid[x, y] = l
+
+
+    l_x = aiGrid[:, :, :, 0].reshape(-1)
+    l_y = aiGrid[:, :, :, 1].reshape(-1)
+    l_z = aiGrid[:, :, :, 2].reshape(-1)
+
+    # Handle out of bounds
+    x_max = video.shape[0]
+    y_max = video.shape[1]
+    z_max = video.shape[2]
+
+
+    m = (l_x >= 0) & (l_x < x_max) & (l_y >= 0) & (l_y < y_max) & (l_z >= 0) & (l_z < z_max)
+
+    l_x[~m] = l_y[~m] = l_z[~m] = 0
+
+    anSample = video[l_x, l_y, l_z]
+
+    anSample = anSample.reshape(aiGrid.shape[0], aiGrid.shape[1], aiGrid.shape[2], 3)
+
+    anSample = np.squeeze(anSample)
+
+    return anSample
+
+
+
+#####################################################
 
 
 input_dir = get_test_dir()
@@ -104,23 +167,17 @@ l_files = list (sorted(input_dir.iterdir()))
 
 l_filenames = [str(x.name) for x in l_files]
 
-video = read_video(l_files[197], 32)
-
-faces = find_two_consistent_faces(mtcnn_detector, video)
+video = read_video(l_files[109], 32)
 
 
-anBegin = etcf(mtcnn_detector, video[0].copy(), faces[0], True)
-anEnd   = etcf(mtcnn_detector, video[31].copy(), faces[1], True)
+rW = 3.0  # feature slack a
 
-# 0,0
-
-p0 = anBegin[0, 0]
-p1 = anEnd[0, 0]
+raster_w = 10
+raster_h = 1
 
 
-get_line(np.array((*p0, 0)), np.array((*p1, 31))).shape
+s = sample(video, mtcnn_detector, rW, raster_w, raster_h, True)
 
 
-# ...
 
 
