d77e6d51f348fe2045f452fd815d3f715b83d13a
poke53280 <anders.topper@gmail.com>
2020-02-26 21:48:14 +0100

    image sampler near complete

--- deepfake/image_sampler.py
+++ deepfake/image_sampler.py
@@ -15,6 +15,8 @@ import matplotlib.pyplot as plt
 import cv2
 
 from easy_face import create_diff_image
+from PIL import Image
+
 
 
 ####################################################################################
@@ -35,139 +37,118 @@ def adjust_box_1d(c, half_size, extent):
     return (b_max + b_min) // 2
 
 
+####################################################################################
+#
+#   process_part
+#
 
-outputsize = 101
+def process_part(iPart):
 
-mtcnn_detector = MTCNNDetector()
+    outputsize = 101
 
-iPart = 6
+    mtcnn_detector = MTCNNDetector()
 
-video_size = 32
+    video_size = 32
 
-W = 256
-H = 1
+    W = 256
+    H = 1
 
-part_dir = get_part_dir(iPart)
+    part_dir = get_part_dir(iPart)
 
-l_d = read_metadata(iPart)
+    l_d = read_metadata(iPart)
 
-entry = l_d[6]
+    for entry in l_d:
 
-orig_path = entry[0]
-fake_path0 = entry[1][0]
+        orig_path = entry[0]
 
-
-orig_video = read_video(part_dir / orig_path, video_size)
-fake_video = read_video(part_dir / fake_path0, video_size)
-
-
-bb_min, bb_max = find_middle_face_box(mtcnn_detector, orig_video)
-
-rDiagnonal = (bb_max - bb_min) * (bb_max - bb_min)
-
-n_characteristic_face_size = np.sqrt(rDiagnonal[0] + rDiagnonal[1]).astype(np.int32)
-
-image_real = orig_video[int(video_size/2)].copy()
+        orig_video = read_video(part_dir / orig_path, video_size)
 
 
-x_max = image_real.shape[1]
-y_max = image_real.shape[0]
+        bb_min, bb_max = find_middle_face_box(mtcnn_detector, orig_video)
 
-sample_size = np.max([int(1.5 * outputsize), 1.5 * n_characteristic_face_size])
-half_size = int (sample_size/2)
+        rDiagnonal = (bb_max - bb_min) * (bb_max - bb_min)
 
-center = 0.5 * (bb_min + bb_max)
-center = center.astype(np.int32)
+        n_characteristic_face_size = np.sqrt(rDiagnonal[0] + rDiagnonal[1]).astype(np.int32)
 
-center_adjusted = np.array([adjust_box_1d(center[0], half_size, x_max), adjust_box_1d(center[1], half_size, y_max)])
-
-s_min = center_adjusted - half_size
-s_max = center_adjusted + half_size
+        image_real = orig_video[int(video_size/2)].copy()
 
 
-image_real = cv2.rectangle(image_real, (s_min[0], s_min[1]), (s_max[0], s_max[1]), (255,0,0), 5)
-plt.imshow(image_real)
-plt.show()
+        x_max = image_real.shape[1]
+        y_max = image_real.shape[0]
 
-# Sample frame
+        sample_size = np.max([int(1.5 * outputsize), 1.5 * n_characteristic_face_size])
+        half_size = int (sample_size/2)
 
-for x in range(4):
+        center = 0.5 * (bb_min + bb_max)
+        center = center.astype(np.int32)
 
-    print (x)
+        center_adjusted = np.array([adjust_box_1d(center[0], half_size, x_max), adjust_box_1d(center[1], half_size, y_max)])
 
-    offset_min = s_min
-    offset_max = s_max - outputsize
-
-    offset_range = offset_max - offset_min
-
-    offset_lo = np.array([offset_min[0] + np.random.choice(offset_range[0]), offset_min[1] + np.random.choice(offset_range[1])])
-
-    offste_hi = offset_lo + outputsize
+        s_min = center_adjusted - half_size
+        s_max = center_adjusted + half_size
 
 
-    #image_real = cv2.rectangle(image_real, (offset_lo[0], offset_lo[1]), (offste_hi[0], offste_hi[1]), (255,0,0), 5)
+        image_real = cv2.rectangle(image_real, (s_min[0], s_min[1]), (s_max[0], s_max[1]), (255,0,0), 5)
+        plt.imshow(image_real)
+        plt.show()
 
-    #plt.imshow(image_real)
-    #plt.show()
+        # Sample frame
+
+        l_test_path = entry[1]
+        l_test_path.append(orig_path)
+
+        for path in l_test_path:
+
+            test_video = read_video(part_dir / path, video_size)
+
+            for x in range(16):
+
+                filename = f"{orig_path[:-4]}_{path[:-4]}_{x}"
 
 
-    real_sample = orig_video[int(video_size/2)][offset_lo[1]:offste_hi[1], offset_lo[0]:offste_hi[0]].copy()
-    fake_sample = fake_video[int(video_size/2)][offset_lo[1]:offste_hi[1], offset_lo[0]:offste_hi[0]].copy()
+                offset_min = s_min
+                offset_max = s_max - outputsize
+
+                offset_range = offset_max - offset_min
+
+                offset_lo = np.array([offset_min[0] + np.random.choice(offset_range[0]), offset_min[1] + np.random.choice(offset_range[1])])
+
+                offste_hi = offset_lo + outputsize
+
+                sample_frame = np.random.choice(video_size)
+
+                real_sample = orig_video[sample_frame][offset_lo[1]:offste_hi[1], offset_lo[0]:offste_hi[0]].copy()
+                test_sample = test_video[sample_frame][offset_lo[1]:offste_hi[1], offset_lo[0]:offste_hi[0]].copy()
+    
+                image_3 = np.sum((real_sample-test_sample)**2,axis=2)
+
+                mask = image_3 > 300
+
+                empty_img = np.zeros((mask.shape[1], mask.shape[0], 3), np.uint8)
+                empty_img[mask] = (255, 0, 0)
+
+                img_tmp = cv2.cvtColor(empty_img, cv2.COLOR_BGR2RGB)
+
+                kernel = np.ones((2,2),np.uint8)
+
+                img_tmp = cv2.dilate(img_tmp,kernel,iterations = 2)
+
+                img_tmp = cv2.erode(img_tmp,kernel,iterations = 2)
+
+                erosion = cv2.cvtColor(img_tmp, cv2.COLOR_RGB2BGR)
+
+
+                real_sample = cv2.cvtColor(real_sample, cv2.COLOR_RGB2GRAY)
+                test_sample = cv2.cvtColor(test_sample, cv2.COLOR_RGB2GRAY)
+
+                im = Image.fromarray(test_sample)
+                im.save(get_output_dir() / (filename +".png"))
+
+                m = erosion == 0
+
+                m = m.all(axis = 2)
+
+                im = Image.fromarray(m)
+                im.save(get_output_dir() / (filename +"_mask.png"))
 
     
-
-    #image_diff = create_diff_image(real_sample, fake_sample)
-    #plt.imshow(image_diff)
-    #plt.show()
-
-
-    image_3 = np.sum((real_sample-fake_sample)**2,axis=2)
-
-    mask = image_3 > 300
-
-    #plt.imshow(mask)
-    #plt.show()
-
-    empty_img = np.zeros((mask.shape[1], mask.shape[0], 3), np.uint8)
-    empty_img[mask] = (255, 0, 0)
-
-    img_tmp = cv2.cvtColor(empty_img, cv2.COLOR_BGR2RGB)
-
-    kernel = np.ones((2,2),np.uint8)
-
-    img_tmp = cv2.dilate(img_tmp,kernel,iterations = 2)
-
-    img_tmp = cv2.erode(img_tmp,kernel,iterations = 2)
-
-    erosion = cv2.cvtColor(img_tmp, cv2.COLOR_RGB2BGR)
-
-
-    #plt.imshow(empty_img)
-    #plt.show()
-    
-    #plt.imshow(erosion)
-    #plt.show()
-
-    m = erosion == 0
-
-    m = m.all(axis = 2)
-
-    plt.imshow(~m)
-    plt.show()
-   
-
-    # Save mask: m
-    # Save fake: fake_sample
-    # Save real: real_sample
-
-    # Fix
-
-
-    real_sample = cv2.cvtColor(real_sample, cv2.COLOR_RGB2GRAY)
-    fake_sample = cv2.cvtColor(fake_sample, cv2.COLOR_RGB2GRAY)
-
-    plt.imshow(real_sample)
-    plt.show()
-
-    plt.imshow(fake_sample)
-    plt.show()
